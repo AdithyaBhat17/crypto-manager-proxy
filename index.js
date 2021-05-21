0a00d4dc639d9c4f08cd98e4852ad5d93f484040
Adithya NR <adithyabhat17.ab@gmail.com>
2021-05-21 17:17:13 +0530

    Added CORS support for adithyabhat.com

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://mudrex.vercel.app"],
+    origin: ["http://localhost:3000", "https://mudrex.vercel.app", /https:\/\/[a-z]+.adithyabhat.com/u],
   })
 );
 
